8204938f68c1b9ff70f3c13b30c97d5f2096a156
Luis Hong <luishong50@gmail.com>
2022-10-06 23:44:03 -0500

    alternative solution found for problem 5; trying to understand it

--- Problem5/SmallestMultiple.js
+++ Problem5/SmallestMultiple.js
@@ -11,39 +11,57 @@
 // }
 // // console.log(evenArray)
 //using brute force
-let found = false
-let smallest = 20
-while(!found){
-    if(
-        smallest%1 == 0 &&
-        smallest%2 == 0 &&
-        smallest%3 == 0 &&
-        smallest%4 == 0 &&
-        smallest%5 == 0 &&
-        smallest%6 == 0 &&
-        smallest%7 == 0 &&
-        smallest%8 == 0 &&
-        smallest%9 == 0 &&
-        smallest%10 == 0 &&
-        smallest%11 == 0 &&
-        smallest%12 == 0 &&
-        smallest%13 == 0 &&
-        smallest%14 == 0 &&
-        smallest%15 == 0 &&
-        smallest%16 == 0 &&
-        smallest%17 == 0 &&
-        smallest%18 == 0 &&
-        smallest%19 == 0 &&
-        smallest%20 == 0 
-    ){
-        console.log("found it", smallest)
-        found = true
-        return
-    }
-    else{
-        smallest++
-    }
+// let found = false
+// let smallest = 20
+// while(!found){
+//     if(
+//         smallest%1 == 0 &&
+//         smallest%2 == 0 &&
+//         smallest%3 == 0 &&
+//         smallest%4 == 0 &&
+//         smallest%5 == 0 &&
+//         smallest%6 == 0 &&
+//         smallest%7 == 0 &&
+//         smallest%8 == 0 &&
+//         smallest%9 == 0 &&
+//         smallest%10 == 0 &&
+//         smallest%11 == 0 &&
+//         smallest%12 == 0 &&
+//         smallest%13 == 0 &&
+//         smallest%14 == 0 &&
+//         smallest%15 == 0 &&
+//         smallest%16 == 0 &&
+//         smallest%17 == 0 &&
+//         smallest%18 == 0 &&
+//         smallest%19 == 0 &&
+//         smallest%20 == 0 
+//     ){
+//         console.log("found it", smallest)
+//         found = true
+//         return
+//     }
+//     else{
+//         smallest++
+//     }
         
 
-}
+// }
 // to do: find more optimal approach
+const check = (num) => {
+    for( let currentDivisor = 1; currentDivisor < 21; currentDivisor++){
+        if((num % currentDivisor) != 0){
+            num = (num * currentDivisor)/(currentDivisor-1)
+            // console.log(num)
+            return num
+        }
+    }
+    console.log(num)
+
+    return 0
+}
+
+let num = 1
+while(check(num) != 0){
+    num = check(num)
+}
+
