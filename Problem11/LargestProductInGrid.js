0996733d0f389f896d12584f111014bd6e6f257d
Luis Hong <luishong50@gmail.com>
2022-10-25 01:08:44 -0500

    problem 11 passed, needs clean up

--- Problem11/LargestProductInGrid.js
+++ Problem11/LargestProductInGrid.js
@@ -48,10 +48,100 @@ let matrix = [
     [01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48]
 ]
 
+let mPrime = [
+    [08,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91,08],
+    [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62,00],
+    [81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,03,49,13,36,65],
+    [52,70,95,23,04,60,11,42,69,24,68,56,01,32,56,71,37,02,36,91],
+    [22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80],
+    [24,47,32,60,99,03,45,02,44,75,33,53,78,36,84,20,35,17,12,50],
+    [32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70],
+    [67,26,20,68,02,62,12,20,95,63,94,39,63,08,40,91,66,49,94,21],
+    [24,55,58,05,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72],
+    [21,36,23,09,75,00,76,44,20,45,35,14,00,61,33,97,34,31,33,95],
+    [78,17,53,28,22,75,31,67,15,94,03,80,04,62,16,14,09,53,56,92],
+    [16,39,05,42,96,35,31,47,55,58,88,24,00,17,54,24,36,29,85,57],
+    [86,56,00,48,35,71,89,07,05,44,44,37,44,60,21,58,51,54,17,58],
+    [19,80,81,68,05,94,47,69,28,73,92,13,86,52,17,77,04,89,55,40],
+    [04,52,08,83,97,35,99,16,07,97,57,32,16,26,26,79,33,27,98,66],
+    [88,36,68,87,57,62,20,72,03,46,33,67,46,55,12,32,63,93,53,69],
+    [04,42,16,73,38,25,39,11,24,94,72,18,08,46,29,32,40,62,76,36],
+    [20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74,04,36,16],
+    [20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54],
+    [01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48]
+]
+
 let rows = matrix.length
 let cols =  matrix[0].length
 
 // console.log(rows, cols)
+const revMatrix = ()=> {
+    let newMatrix = []
+    // let mPrime = matrix
+    for(let i = 0; i <= rows -1 ; i++){
+        let revRow = mPrime[i].reverse()
+        // console.log(revRow)
+        newMatrix.push(revRow)
+    }
+    // console.log(newMatrix)
+    return newMatrix
+}
+let mirrorMatrix = revMatrix()
+
+const largestDiagonalRightToLeft = () => {
+    let largestInMatrix = 0
+    let largestDiag = 0
+    let set = []
+
+    for(let indDiag = 0; indDiag <= rows -1; indDiag++){
+        for(let i = 0, j = 1, k = 2, l = 3; (l <= rows - 1) && (indDiag+3 < rows); l++, i++, j++ , k++){
+            
+            let first =  mirrorMatrix[indDiag][i]
+            let second = mirrorMatrix[indDiag+1][j]
+            let third = mirrorMatrix[indDiag+2][k]
+            let fourth =  mirrorMatrix[indDiag+3][l]
+            // console.log(first, second, third, fourth)
+            let diagProduct = first*second*third*fourth
+            if(largestInMatrix < diagProduct){
+                set = [first,second,third,fourth]
+                largestInMatrix = diagProduct
+            }
+        }
+        // console.log("------------0-----------")
+    }
+    console.log(largestInMatrix,"right to left")
+    // console.log(set)
+}
+largestDiagonalRightToLeft()
+
+
+
+const largestDiagonalLeftToRight = ()=>{
+    let largestInMatrix = 0
+    let largestDiag = 0
+    let set = []
+    // console.log(matrix)
+    for(let indDiag = 0; indDiag <= rows -1; indDiag++){
+        for(let i = 0, j = 1, k = 2, l = 3; (l <= rows - 1) && (indDiag+3 < rows); l++, i++, j++ , k++){
+            
+            let first =  matrix[indDiag][i]
+            let second = matrix[indDiag+1][j]
+            let third = matrix[indDiag+2][k]
+            let fourth =  matrix[indDiag+3][l]
+            // console.log(first, second, third, fourth)
+            let diagProduct = first*second*third*fourth
+            if(largestInMatrix < diagProduct){
+                set = [first,second,third,fourth]
+                largestInMatrix = diagProduct
+            }
+        }
+        // console.log("------------0-----------")
+    }
+    console.log(largestInMatrix,"left to right")
+    // console.log(set)
+}
+largestDiagonalLeftToRight()
+
 
 const largestAcross = ()=>{
     let largestInMatrix = 0
@@ -78,9 +168,9 @@ const largestAcross = ()=>{
             largestInMatrix = largestByRow
         }
     }
-    console.log(largestInMatrix)
+    console.log(largestInMatrix,"larget across")
 }
-// largestAcross()
+largestAcross()
 
 const largestVertical = ()=>{
     // for(let indCol = 0; indCol <= cols -1; indCol++){
@@ -105,11 +195,13 @@ const largestVertical = ()=>{
             largestInMatrix = largestByCol
         }
     }
-    // console.log(largestInMatrix)
+    console.log(largestInMatrix,"largest vertical")
 
     // }
 }
-// largestVertical()
+largestVertical()
+
+
 
 
 for(let i = 0; i <= rows; i++){
