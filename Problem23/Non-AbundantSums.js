bc2ebdceee3eebd5db42a375660bdc2409663104
Luis Hong <luishong50@gmail.com>
2022-11-30 12:12:32 -0600

    problem 24 js passed, ruby in progress

--- Problem23/Non-AbundantSums.js
+++ Problem23/Non-AbundantSums.js
@@ -5,3 +5,32 @@
 // As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 
 // Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+const listDivisors = (n) => {
+    let list = []
+    let noOfDivisors 
+    for(let i = 1; i <= Math.sqrt(n); i++){
+        if(n % i === 0){
+            if(parseInt((n/i), 10) == i){
+                list.push(i)
+            }else {
+                list.push(i)
+                list.push(n/i)
+            }
+        }
+    }
+    noOfDivisors = list.length
+    let indexOfSelf = list.indexOf(n)
+    list.splice(indexOfSelf,1)
+    let posibleAmiNum = list.reduce((accumulator, currentValue) => accumulator + currentValue,0)
+    return posibleAmiNum
+}
+// console.log(listDivisors(24));
+let abundantTotal = 0
+for(let i = 1; i < 220; i++){
+    if(listDivisors(i) >= i){
+        console.log(i);
+        abundantTotal+= i
+    }
+}
+console.log(abundantTotal);
---
+++ Problem24/lexico_permutations.rb
@@ -0,0 +1,14 @@
+result = []
+
+def permute(arr, m = "") 
+    if(arr.length == 0)
+        result.push(m)
+
+    else
+        for i in 0..arr.lenght-1
+            curr = arr.slice()
+            next = curr.splice(i,)
+        end
+
+    end
+end
