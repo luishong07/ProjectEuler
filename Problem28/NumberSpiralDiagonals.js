3110ff70a4e718e3bfbb1550b7fd910e5bab250b
Luis Hong <luishong50@gmail.com>
2023-02-21 16:32:03 -0600

    problem 28 done. but admitedly, spiral algorithm had to be looked up

--- Problem28/NumberSpiralDiagonals.js
+++ Problem28/NumberSpiralDiagonals.js
@@ -11,12 +11,87 @@
 // What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 
 
-let rows = 5
-let columns = 5
+let rows = 1001
+let columns = 1001
 
 let matrix =[]
 
 for (let i=0; i < rows; i++){
     matrix.push([])
 }
-console.log(matrix);
+
+// let arr = [
+//     1, 2, 3, 4, 5, 6,
+//     7, 8, 9, 10, 11, 12,
+//     13, 14, 15, 16, 17, 18,
+//     19, 20, 21, 22, 23, 24, 25
+// ]
+
+let totalCount = rows * columns 
+let arr =[]
+for(let i = totalCount; i >= 1; i--){
+    arr.push(i)
+}
+// console.log(arr);
+//function copied from geeks for geeks
+function MatrixToSpiral(arr, mat){
+    let top = 0
+    let bottom = rows - 1
+    let left = 0
+    let right = columns - 1;
+
+    let index = 0
+
+
+
+    while(true){
+        if(left > right) break
+        //top row
+        for(let i  = left; i<= right; i++){
+            mat[top][i] = arr[index++]
+        }
+        top++
+
+        if(top > bottom) break
+        //right column
+        for(let i = top; i <= bottom;i++){
+            mat[i][right] = arr[index++]
+        }
+        right--
+
+        if(left > right) break
+        //bottom row
+        for(let i = right; i >= left; i--){
+            mat[bottom][i] = arr[index++]
+
+        }
+        bottom--
+
+        if(top > bottom) break
+        //left column
+        for(let i = bottom; i >= top; i--){
+            mat[i][left] = arr[index++]
+        }
+        left++
+    }
+}
+function printSpiral(mat){
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < columns; j++){
+            console.log(mat[i]);
+        }
+    }
+}
+MatrixToSpiral(arr, matrix)
+// printSpiral(matrix)
+// console.log(matrix);
+let diagonal = 0
+let antiDiagonal = 0
+for(let i = 0, d = matrix.length-1 ; i <= matrix.length -1; i++, d--){
+    // console.log(matrix[i][d]);
+    diagonal += matrix[i][i]
+    antiDiagonal += matrix[i][d]
+}
+let total = diagonal + antiDiagonal - 1
+console.log(total);
+
