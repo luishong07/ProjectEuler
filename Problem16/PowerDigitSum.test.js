842fc4302becc12761dc42c6391d45069e6a6ed8
Luis Hong <luishong50@gmail.com>
2023-02-28 12:06:38 -0600

    using a very crude, and brute foce method, but still got the right answer; problem 31 passed

--- Problem16/PowerDigitSum.test.js
+++ Problem16/PowerDigitSum.test.js
@@ -1,6 +1,5 @@
-const PowerDigitSum = require('./PowerDigitSum')
+const PowerDigitSum = require("./PowerDigitSum");
 
-test('What is the sum of the digits of the number 2^1000?',
-    ()=>{
-        expect(PowerDigitSum()).toBe(1366n)
-})
+test("What is the sum of the digits of the number 2^1000?", () => {
+    expect(PowerDigitSum()).toBe(1366n);
+});
--- Problem31/CoinSums.js
+++ Problem31/CoinSums.js
@@ -5,3 +5,45 @@
 
 // 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 // // How many different ways can £2 be made using any number of coins?
+let twoPounds = 200
+let onePound = 100
+let fiftyP = 50
+let twentyP = 20
+let tenP = 10
+let fiveP = 5
+let twoP = 2
+let oneP = 1
+
+let onePoundChange = 2
+let fiftyPChange = 4
+let twentyPChange = 10
+let tenPChange = 20
+let fivePChange = 40
+let twoPChange = 100
+let onePChange = 200
+let sum = 0
+let count = 0
+for(let p100 = 0; p100 <= onePoundChange; p100++){
+    for(let p50 = 0; p50 <= fiftyPChange; p50++){
+        for(let p20 = 0; p20 <= twentyPChange; p20++){
+            for(let p10 = 0; p10<= tenPChange; p10++){
+                for(let p5 = 0; p5 <= fivePChange; p5++){
+                    for(let p2 = 0; p2 <= twoPChange; p2++){
+                        for(let p1 = 0; p1 <= onePChange; p1++){
+
+                            sum = onePound*p100 + fiftyP*p50 + twentyP*p20+tenP*p10+fiveP*p5+twoP*p2+oneP*p1
+                            if(sum == 200){
+                                console.log('success');
+                                // return
+                                console.log(100,p100,50,p50,20,p20,10,p10,5,p5,2,p2,1,p1)
+                                count++
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+console.log(count + 1); //+1 from adding one single 2 pound coin
