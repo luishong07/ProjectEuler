dac344f91d5963296aa64a14d0a53f3a78314087
Luis Hong <luishong50@gmail.com>
2023-03-06 22:18:10 -0600

    problem 33 passed in js

--- Problem33/DigitCancelling.js
+++ Problem33/DigitCancelling.js
@@ -6,45 +6,67 @@
 
 // If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
 const DigitCancel = () => {
-    let count = 0;
-    for (let i = 1; i <= 99; i++) {
-        for (let j = 1; j <= 99; j++) {
+    let countLargerThanOne = 0;
+    let result = 1
+    for (let i = 10; i <= 99; i++) {
+        for (let j = 10; j <= 99; j++) {
             let fraction = i / j;
-            if (fraction >= 1) {
-                count++;
+            if (fraction < 1) {
+                // count++;
                 // console.log(`${i}/${j}`)
-                return
+
+                let stringNumerator = i.toString();
+                let stringDenominator = j.toString();
+
+                let numeratorArray = stringNumerator
+                    .split("")
+                    .map((n) => parseInt(n));
+
+                let denominatorArray = stringDenominator
+                    .split("")
+                    .map((n) => parseInt(n));
+
+
+                let newNumerator = numeratorArray.reduce((accu, current) => {
+                    return accu * current;
+                }, 1);
+
+                let sharedDigits = numeratorArray.some(n => denominatorArray.indexOf(n) >= 0)
+
+                let newDenominator = denominatorArray.reduce(
+                    (accu, current) => {
+                        return accu * current;
+                    },
+                    1
+                );
+
+                let newFration = newNumerator / newDenominator;
+
+                // if (newDenominator == 0) {
+                //     // console.log("Denominator zero");
+                // }
+                // console.log(newFration)
+                
+                if (fraction == newFration && newDenominator != 0 && sharedDigits) {
+                    console.log(sharedDigits, fraction, `${i}/${j}`)
+                    //check for common digits in numerator and denominator
+
+                    result *= fraction
+
+                }
+
+            }else{
+                countLargerThanOne++
             }
-            let stringNumerator = i.toString();
-            let stringDenominator = j.toString();
-
-            let numeratorArray = stringNumerator
-                .split("")
-                .map((n) => parseInt(n));
-            let denominatorArray = stringDenominator
-                .split("")
-                .map((n) => parseInt(n));
-
-            let newNumerator = numeratorArray.reduce((accu, current) => {
-                return accu * current;
-            }, 1);
-
-            let newDenominator = denominatorArray.reduce((accu, current) => {
-                return accu * current;
-            }, 1);
-
-            let newFration = newNumerator / newDenominator;
-
-            // if (newDenominator == 0) {
-            //     return;
-            // }
-
-            // if (fraction == newFration) {
-            //     console.log(`${i}/${j}`)
-
-            // }
         }
     }
-    console.log(count);
+    console.log(result);
 };
-DigitCancel()
+DigitCancel();
+
+// for(let i = 0; i < 10; i++){
+//     if(i == 5){
+//         return
+//     }
+//     console.log(i)
+// }
