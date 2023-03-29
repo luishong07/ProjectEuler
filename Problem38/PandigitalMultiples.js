b9ae7d71141e98ef58b9ad86a185b9b2ea0f1920
Luis Hong <luishong50@gmail.com>
2023-03-29 17:56:11 -0500

    problem with initial function to check if it is pandigital; test for number 18; length of result is 10

--- Problem38/PandigitalMultiples.js
+++ Problem38/PandigitalMultiples.js
@@ -12,38 +12,54 @@ const startNumber = 9;
 const endNumber = 1000;
 
 const pandigital1To9 = (n) => {
-    let isPandigital = true;
+    let checkPan = true;
     let concatResult = "";
     let i = 1;
     let result;
-    while (isPandigital) {
-        // console.log(concatResult)
-        if (concatResult.length >= 9) {
-            isPandigital = false;
-            result = [concatResult, n, i - 1];
+    while (checkPan) {
+        // console.log(concatResult.length)
+        if (concatResult.length > 9) {//if concatResult length is greater than 9 stop the loop
+            break
+            // checkPan = false;
+            // result = [concatResult, n, i - 1];
         } else {
-            concatResult += `${i * n}`;
-            i++;
+            // concatResult += `${i * n}`;
         }
+        i++;
     }
+    console.log(concatResult)
+    // let freq = {};
+    // for (let j = 0; j < concatResult.length; j++) {
+    //     if (!(concatResult[j] in freq)) {
+    //         //if number is not in freq obj
+    //         freq[concatResult[j]] = 1; // add it to the obj and set it to one
+    //     } else {
+    //         //else, it is already there, just add one to it
+    //         freq[concatResult[j]] += 1;
+    //     }
+    // }
 
-    let freq = {};
-    for (let j = 0; j < concatResult.length; j++) {
-        if (!(concatResult[j] in freq)) {
-            //if number is not in freq obj
-            freq[concatResult[j]] = 1; // add it to the obj and set it to one
-        } else {
-            //else, it is already there, just add one to it
-            freq[concatResult[j]] += 1;
-        }
-    }
-
-    console.log(Object.values(freq))
-    
-
+    // console.log(Object.values(freq))
+    // let freqValues = Object.values(freq);
+    // for (let k = 0; k < freqValues.length; k++) {
+    //     if (freqValues[k] != 1) {
+    //         // console.log("problem")
+    //         return false;
+    //     }
+    // }
+    // return parseInt(concatResult)
 };
-pandigital1To9(192);
-// while(startNumber < endNumber){
-//     let result
-
+// console.log(pandigital1To9(18).toString().length)
+pandigital1To9(18)
+// let current = 0;
+// let i = 9;
+// while (current < 987654321) {
+//     console.log(i)
+//     current = pandigital1To9(i)
+//     if(current){
+//         console.log(current)
+//     }else{
+//         console.log(current)
+//     }
+//     i++
 // }
