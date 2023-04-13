259f4d37fe49c75e5374ed20226f5eec542389b4
Luis Hong <luishong50@gmail.com>
2023-04-13 15:52:36 -0500

    started problem 41

---
+++ Problem41/PandigitalPrime.js
@@ -0,0 +1,50 @@
+// We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+
+// What is the largest n-digit pandigital prime that exists?
+
+// let elements = [1,2,3,4]
+// let elements = '1234'
+
+
+const permutator = (inputArr) => {// this returns all possible permutations of the elements in provided array
+    let result = []
+
+    const permute = (arr, m ="")=>{
+        if(arr.length === 0){
+            result.push(parseInt(m))
+        }else{
+            for(let i = 0; i < arr.length; i++){
+                let curr = arr.slice()
+                let next = curr.splice(i, 1)
+                permute(curr.slice(), m.concat(next))
+            }
+        }
+    }
+    permute(elements)
+    return result
+
+}
+// console.log(permutator(elements))
+const isPrime = (n)=>{
+    if(n <= 1) return false
+
+    if(n == 2 || n == 3) return true
+
+    if(n % 2 == 0 || n % 3 == 0) return false
+
+    for(let i = 5; i <= Math.sqrt(n); i = i+6){
+        if(n % i == 0 || n % (i+2) ==0){
+            return false
+        }
+    }
+    return true
+}
+
+let elements = [1]
+let i = 2
+while(i < 1000){
+    let permutations = permutator(elements)
+    for(let j = 0; j < permutations.length; j++){
+        
+    }
+}
